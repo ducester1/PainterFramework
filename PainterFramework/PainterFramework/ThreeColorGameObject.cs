d0cb88ec54eb1cff6508898253eedb36e2d72be0
Scott Schepers <schepers.scott@hotmail.com>
2017-03-07 09:57:34 +0100

    C1 completed

--- PainterFramework/PainterFramework/ThreeColorGameObject.cs
+++ PainterFramework/PainterFramework/ThreeColorGameObject.cs
@@ -18,7 +18,31 @@ namespace PainterFramework
                 colorGreen = new SpriteSheet(greenAssetName);
                 colorBlue = new SpriteSheet(blueAssetName);
 
-                color = Color.Blue;
+                Color = Color.Blue;
             }
+
+        public virtual void Reset()
+        {
+            base.Reset();
+
+            Color = Color.Blue;
+        }
+
+        public Color Color
+        {
+            get { return color; }
+            set
+            {
+                if (value != Color.Red && value != Color.Green && value != Color.Blue)
+                    return;
+                color = value;
+                if (color == Color.Red)
+                    sprite = colorRed;
+                else if (color == Color.Green)
+                    sprite = colorGreen;
+                else if (color == Color.Blue)
+                    sprite = colorBlue;
+            }
+        }
     }
 }
