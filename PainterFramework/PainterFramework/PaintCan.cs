d12fadc8e58641a403bfe5d11e3ba699da38cbbd
Scott Schepers <schepers.scott@hotmail.com>
2017-03-07 12:37:34 +0100

    D1 completed

---
+++ PainterFramework/PainterFramework/PaintCan.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PainterFramework
+{
+    class PaintCan : ThreeColorGameObject
+    {
+        protected Color targetColor;
+        protected float minVertVelocity;
+        protected float horizontalPosOff;
+
+        public PaintCan(float horizontalPosOff, Color targetColor)
+            :base("spr_can_red", "spr_can_green", "spr_can_blue")
+        {
+            this.horizontalPosOff = horizontalPosOff;
+            this.targetColor = targetColor;
+
+            minVertVelocity = 30;
+        }
+    }
+}
