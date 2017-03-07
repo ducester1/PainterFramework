942df1026df603601c10519e40e6df062ef89147
Scott Schepers <schepers.scott@hotmail.com>
2017-03-07 21:19:40 +0100

    E1 completed

---
+++ PainterFramework/PainterFramework/Ball.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PainterFramework
+{
+    class Ball : ThreeColorGameObject
+    {
+        public bool Shooting { get; set; }
+        public Ball()
+            :base("spr_ball_red","spr_ball_green","spr_ball_blue")
+        {
+
+        }
+
+        public override void Reset()
+        {
+            Visible = false;
+            velocity = Vector2.Zero;
+            Shooting = false;
+            base.Reset();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (Shooting == true)
+            {
+                velocity.X *= .99f;
+                velocity.Y += 6;
+            }
+            base.Update(gameTime);
+        }
+    }
+}
