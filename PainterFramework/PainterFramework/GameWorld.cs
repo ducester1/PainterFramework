1fb49f33dcfbe9c065df2cbec7fd55906d05ab7f
Scott Schepers <schepers.scott@hotmail.com>
2017-03-07 10:59:23 +0100

    C4 completed

--- PainterFramework/PainterFramework/GameWorld.cs
+++ PainterFramework/PainterFramework/GameWorld.cs
@@ -10,14 +10,18 @@ namespace PainterFramework
     class GameWorld : GameObjectList
     {
         private SpriteGameObject background;
-        private SpriteGameObject cannonBarrel;
+        private RotatableSpriteGameObject cannonBarrel;
         private ThreeColorGameObject cannonColor;
 
         public GameWorld()
         {
             background = new SpriteGameObject("spr_background");
-            cannonBarrel = new SpriteGameObject("spr_cannon_barrel");
+            cannonBarrel = new RotatableSpriteGameObject("spr_cannon_barrel");
+            cannonBarrel.Position = new Vector2(74, 404);
+            cannonBarrel.Origin = new Vector2(34, 34);
+
             cannonColor = new ThreeColorGameObject("spr_cannon_red", "spr_cannon_green", "spr_cannon_blue");
+            cannonColor.Position = new Vector2(58, 388);
 
             this.Add(background);
             this.Add(cannonBarrel);
@@ -31,6 +35,10 @@ namespace PainterFramework
             if (inputHelper.KeyPressed(Keys.R)) cannonColor.Color = Color.Red;
             else if (inputHelper.KeyPressed(Keys.G)) cannonColor.Color = Color.Green;
             else if (inputHelper.KeyPressed(Keys.B)) cannonColor.Color = Color.Blue;
+
+            double opposite = inputHelper.MousePosition.Y - cannonBarrel.GlobalPosition.Y;
+            double adjacent = inputHelper.MousePosition.X - cannonBarrel.GlobalPosition.X;
+            cannonBarrel.Angle = (float)Math.Atan2(opposite, adjacent);
         }
     }
 }
---
+++ PainterFramework/PainterFramework/RotatableSpriteGameObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PainterFramework
+{
+    class RotatableSpriteGameObject : SpriteGameObject
+    {
+        protected float angle;
+        public RotatableSpriteGameObject(String assetname, int layer = 0, string id = "", int sheetIndex = 0)
+            :base (assetname, layer, id, sheetIndex)
+            {
+                
+            }
+
+        public float Angle { get { return angle; } set { angle = value; } }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            if (Visible != true || sprite == null) return;
+            spriteBatch.Draw(sprite.Sprite, this.GlobalPosition, null, Color.White, angle, this.origin, 1.0f, SpriteEffects.None, 0);
+        }
+    }
+}
