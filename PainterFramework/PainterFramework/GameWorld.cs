cdda4a230b761a7ca1ca43a4937aed40223f14ea
Scott Schepers <schepers.scott@hotmail.com>
2017-03-07 10:04:57 +0100

    C2 completed

--- PainterFramework/PainterFramework/GameWorld.cs
+++ PainterFramework/PainterFramework/GameWorld.cs
@@ -8,12 +8,18 @@ namespace PainterFramework
     class GameWorld : GameObjectList
     {
         private SpriteGameObject background;
+        private SpriteGameObject cannonBarrel;
+        private ThreeColorGameObject cannonColor;
 
         public GameWorld()
         {
             background = new SpriteGameObject("spr_background");
+            cannonBarrel = new SpriteGameObject("spr_cannon-barrel");
+            cannonColor = new ThreeColorGameObject("spr_cannon_red", "spr_cannon_green", "spr_cannon_blue");
 
             this.Add(background);
+            this.Add(cannonBarrel);
+            this.Add(cannonColor);
         }
     }
 }
