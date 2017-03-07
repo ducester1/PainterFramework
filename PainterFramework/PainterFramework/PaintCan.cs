8abb8a28dcefb104d2dc783e641c7a28501331b4
Scott Schepers <schepers.scott@hotmail.com>
2017-03-07 20:40:06 +0100

    D4 completed

--- PainterFramework/PainterFramework/PaintCan.cs
+++ PainterFramework/PainterFramework/PaintCan.cs
@@ -22,11 +22,45 @@ namespace PainterFramework
             Reset();
         }
 
+        public override void Update(GameTime gameTime)
+        {
+            if (velocity.Y <= 0f && GameEnvironment.Random.NextDouble() < 0.01)
+            {
+                Velocity = CalculateRandomVelocity();
+                Color = CalculateRandomColor();
+            }
+
+            minVertVelocity += 0.001f;
+            base.Update(gameTime);
+        }
+
         public override void Reset()
         {
             base.Reset();
             position = new Vector2(this.horizontalPosOff, -BoundingBox.Height);
             velocity = Vector2.Zero;
+        }
+
+        public Vector2 CalculateRandomVelocity()
+        {
+            return new Vector2(0f,(float)GameEnvironment.Random.NextDouble()*30 + minVertVelocity);
+        }
+
+        public Color CalculateRandomColor()
+        {
+            int RandomColor = GameEnvironment.Random.Next(0, 3);
+
+            switch (RandomColor)
+            {
+                case 1:
+                    return Color.Red;
+                case 2:
+                    return Color.Green;
+                case 3:
+                    return Color.Blue;
+                default:
+                    return Color.Red;
+            }
         }
     }
 }
