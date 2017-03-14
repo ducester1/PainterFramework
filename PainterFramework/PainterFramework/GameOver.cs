dcaf3684a51f7d42deaca2647653502217b87e79
Scott Schepers <schepers.scott@hotmail.com>
2017-03-14 19:47:14 +0100

    I completed

---
+++ PainterFramework/PainterFramework/GameOver.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace PainterFramework
+{
+    class GameOver : GameObjectList
+    {
+        TextGameObject text;
+        public GameOver()
+            {
+            text = new TextGameObject("GameFont");
+
+            this.Add(text);
+
+            text.Text = "Game over dude! Game over!";
+            text.Position = new Vector2(200, Painter.Screen.Y / 2);
+            
+            }
+    }
+}
